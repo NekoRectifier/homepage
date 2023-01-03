14428624fdd354182a967741f8622858ec32f65a
NekoRectifier <hyx123250@gmail.com>
2023-01-03 22:54:17 +0800

    feat: update title

--- src/App.js
+++ src/App.js
@@ -3,10 +3,17 @@ import './App.css';
 import {Card, CardActions, CardContent, IconButton, Typography} from "@mui/material";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import TelegramIcon from "@mui/icons-material/Telegram";
+import {Helmet} from "react-helmet";
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <meta charSet={"utf-8"}/>
+        <title>NekøRect</title>
+        <link rel={"icon"} type={"image/png"} href={"https://blog.nekorect.eu.org/images/logo_round.png"}></link>
+      </Helmet>
+    
       <Card className={"Main"}>
         <CardContent>
           <div className={"NameDisplay"}>
