177af7e9c824f8335e56756ac774bf74c4780493
NekoRectifier <hyx123250@gmail.com>
2023-01-05 14:45:25 +0800

    fix: (maybe) card margin and width

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import {
+  Box,
   Button,
   Card,
   CardActions,
   CardContent,
   CardMedia,
+  Container,
   createTheme,
   IconButton,
   ThemeProvider,
@@ -25,52 +27,56 @@ const darkTheme = createTheme({
 function App() {
   return (
     <ThemeProvider theme={darkTheme}>
-      <div className="App">
-        <Helmet>
-          <meta charSet={"utf-8"}/>
-          <title>NekøRect</title>
-          <link rel={"icon"} type={"image/png"} href={"https://blog.nekorect.eu.org/images/logo_round.png"}></link>
-        </Helmet>
+    
+      <Helmet>
+        <meta charSet={"utf-8"}/>
+        <title>NekøRect</title>
+        <link rel={"icon"} type={"image/png"} href={"https://blog.nekorect.eu.org/images/logo_round.png"}></link>
+      </Helmet>
+    
+      <Container maxWidth={"lg"}>
+        <Box className={"App"}>
         
-        <Card className={"Main"}>
+          <Card className={"Main"}>
+            <CardMedia
+              sx={{height: 250}}
+              image="../static/images/info_bg.png"
+            />
           
-          <CardMedia
-            sx={{height: 250}}
-            image="../static/images/info_bg.png"
-          />
-          
-          <CardContent>
-            <div className={"NameDisplay"}>
-              <Typography variant={"h3"}>
-                NekoRectifier
-              </Typography>
-              <Typography color="text.secondary">
-                a.k.a. NekoRect
-              </Typography>
-            </div>
+            <CardContent>
+              <div className={"NameDisplay"}>
+                <Typography variant={"h3"}>
+                  NekoRectifier
+                </Typography>
+                <Typography color="text.secondary">
+                  a.k.a. NekoRect
+                </Typography>
+              </div>
             
-            {/*  add a split line*/}
-            <hr></hr>
-            <Typography>
-              A 21 yo. Undergraduate who is passionate about coding and open sourcing.
-            </Typography>
+              {/*  add a split line*/}
+              <hr></hr>
+              <Typography>
+                A 21 yo. Undergraduate who is passionate about coding and open sourcing.
+              </Typography>
           
-          </CardContent>
+            </CardContent>
           
-          <div className={"ShortActions"}>
-            <Button href={"https://blog.nekorect.eu.org/"}>Blog</Button>
-            <Button href={"https://tool.nekorect.eu.org/"}>Toolbox</Button>
-          </div>
+            <div className={"ShortActions"}>
+              <Button href={"https://blog.nekorect.eu.org/"}>Blog</Button>
+              <Button href={"https://tool.nekorect.eu.org/"}>Toolbox</Button>
+            </div>
           
-          <CardActions sx={{justifyContent: 'flex-end'}}>
-            <IconButton color={"inherit"} href={"https://nekorect.eu.org/about"}><InfoIcon/></IconButton>
-            <IconButton color={'inherit'} href={"https://github.com/NekoRectifier"}><GitHubIcon/></IconButton>
-            <IconButton color={'inherit'} href={"https://t.me/NekoRectifier"}><TelegramIcon/></IconButton>
-            <IconButton color={"inherit"} href={"https://twitter.com/NekoKjswg5Q"}><Twitter/></IconButton>
-          </CardActions>
+            <CardActions sx={{justifyContent: 'flex-end'}}>
+              <IconButton color={"inherit"} href={"https://nekorect.eu.org/about"}><InfoIcon/></IconButton>
+              <IconButton color={'inherit'} href={"https://github.com/NekoRectifier"}><GitHubIcon/></IconButton>
+              <IconButton color={'inherit'} href={"https://t.me/NekoRectifier"}><TelegramIcon/></IconButton>
+              <IconButton color={"inherit"} href={"https://twitter.com/NekoKjswg5Q"}><Twitter/></IconButton>
+            </CardActions>
         
-        </Card>
-      </div>
+          </Card>
+        </Box>
+      </Container>
+  
     </ThemeProvider>
   );
 }
