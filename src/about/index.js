48043f6d36cd68f65948a0fe196da5cabaf3083f
NekoRectifier <hyx123250@gmail.com>
2023-01-06 15:17:40 +0800

    rm: del all trace code and unwanted files

--- src/about/index.js
+++ src/about/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
-import './index.css';
-import App from './App';
 import {createRoot} from "react-dom/client";
+import {Typography} from "@mui/material";
+
+function App() {
+  return (
+    <Typography>
+      hi
+    </Typography>
+  );
+  
+}
 
 const container = document.getElementById('root')
 const root = createRoot(container)
 root.render(<App/>)
+
+reportWebVitals(sendToVercelAnalytics);
